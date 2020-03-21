504abd4bb509373010de96e7312f0aa46a892404
andrana <HJandrij@gmail.com>
2020-03-21 11:57:07 +0300

    Updates

---
+++ precache-manifest.e0409041b8af1a056f9617ee1783f1f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efb048826a86859e71b2a15db8a8e50",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "885a4f9505d9391bb37b",
+    "url": "/robot/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "73a591b0174dd8836fa0",
+    "url": "/robot/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "885a4f9505d9391bb37b",
+    "url": "/robot/static/js/2.35164f70.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robot/static/js/2.35164f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a591b0174dd8836fa0",
+    "url": "/robot/static/js/main.a91c9f9c.chunk.js"
+  },
+  {
+    "revision": "1ead1339950e250c33da",
+    "url": "/robot/static/js/runtime-main.846be3a3.js"
+  }
+]);
