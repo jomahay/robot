4e97c76440c7e232a9f311433e0b838162b02ab0
andrana <HJandrij@gmail.com>
2020-03-21 11:44:00 +0300

    alefa barea

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component{
+
+    constructor(props){
+
+
+        super(props);
+        this.state={
+
+            hasError:false
+        }
+    }
+
+    componentDidCatch(error,info){
+
+        this.setState({hasError:true});
+
+    }
+
+    render(){
+
+        if(this.state.hasError){
+
+            return <h1>Oooops , mis blem zan </h1>
+
+        }
+
+        return this.props.children;
+         
+    }
+
+
+}
+
+export default ErrorBoundry;
